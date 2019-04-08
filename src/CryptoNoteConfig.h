3b4b68f4430d0fdb2e79cda23d04a412e50cac42
zlatkomajstor <33296664+zlatkomajstor@users.noreply.github.com>
2019-04-08 22:05:59 +0200

    Change USA node

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -157,7 +157,7 @@ const uint32_t P2P_IDLE_CONNECTION_KILL_INTERVAL             = (5 * 60);      //
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "8f80f9a5a434a9f1510d13336228debfee9c918ce505efe225d8c94d045fa115";
 
 const char* const SEED_NODES[] = {
-  "192.169.140.225:37777",
+  "155.138.224.33:37777",
   "95.179.196.57:37777",
   "95.179.145.244:37777",
   "45.32.103.146:37777"
